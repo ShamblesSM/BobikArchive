fb8b454adf5b508f22d589aae907700014343b31
Shambles_SM <115082072+ShamblesSM@users.noreply.github.com>
2023-03-16 13:14:47 +0800

    Initial commit
    - Added base HTML files
    - Added 5 of Bobik's works to the database ~~ehem restored those to Irminsul~~

---
+++ assets/js/item.js
@@ -0,0 +1,79 @@
+const qs = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+""
+function getMetaInfoString(field, word) {
+    return field ? field.replace(/\n/g, "<br>") : `<i>No ${word} provided.</i>`;
+}
+
+$.get("/assets/master_database.json", (data) => {
+    const MASTER_DB = data;
+    let item_meta;
+
+    if (MASTER_DB.works[qs.id]) {
+        item_meta = MASTER_DB.works[qs.id];
+    } else {
+        window.location.replace("/404");
+    }
+
+    // display name, description & notes
+    document.getElementById("item-name").innerHTML = item_meta.displayName;
+    document.getElementById("item-description").innerHTML = getMetaInfoString(item_meta.description, "description");
+    document.getElementById("item-notes").innerHTML = getMetaInfoString(item_meta.notes, "notes");
+    
+    // images
+    if (item_meta.images || item_meta.images > 0) {
+        let images = document.getElementById("item-images");
+        item_meta.images.forEach((url) => {
+            let child = document.createElement("img");
+            child.src = `assets/db_files/${qs.id}/${url}`
+            child.style.width = "300px";
+            child.style.margin = "2px";
+            images.appendChild(child)
+        });
+    } else {
+        document.getElementById("item-images").innerHTML = '<i>No images provided.</i>';
+    }
+
+    // information
+    const INFO_TABLE = document.getElementById("item-info");
+    const INFO_ITEMS = ["medium", "originalDate", "archiveDate", "originalSource"];
+    const INFO_ITEMS_D = {
+        medium: "Medium",
+        originalDate: "Original Date",
+        archiveDate: "Archival Date",
+        originalSource: "Original Source"
+    }
+    INFO_ITEMS.forEach(key => {
+        if (!item_meta[key]) {
+            return;
+        }
+        let row = INFO_TABLE.insertRow();
+        let contents = item_meta[key]
+
+        if (/^(original|archive)Date$/.exec(key)) {
+            let date = new Date(contents)
+            contents = `<span title="${date.toDateString()}" class="hastooltip">${contents}</span>`
+        } else if (key == "originalSource") {
+            contents = `<a href="${contents}" title="${contents}">link</a>`
+        }
+
+        $(row).append(`<th>${INFO_ITEMS_D[key]}</th>`)
+        $(row).append(`<td>${contents}</td>`)
+    });
+
+    // downloads
+    const DOWNLOADS_TABLE = document.getElementById("item-downloads");
+
+
+    item_meta.downloadLinks.forEach(linkData => {
+        let row = DOWNLOADS_TABLE.insertRow();
+        $(row).append(`<th>${linkData.name}</th>`)
+        
+        let notes = ""
+        if (linkData.notes && linkData.notes.trim() != "") {
+            notes = ` (<span title="${linkData.notes}" class="hastooltip">notes</span>)`
+        }
+        $(row).append(`<td><a href="${linkData.link}" title="${linkData.link}">link</a>${notes}</td>`)
+    });
+});
---
+++ assets/js/works.js
@@ -0,0 +1,46 @@
+const qs = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+
+$.get("/assets/master_database.json", (data) => {
+    const MASTER_DB = data;
+
+    const VALID_QS = ["spherematchers", "pekkakana2"]
+    const MEDIUM_KEYS = {
+        spherematchers: "Sphere Matchers",
+        pekkakana2: "Pekka Kana 2"
+    }
+
+    if (!VALID_QS.includes(qs.category)) {
+        window.location.replace("/404");
+    }
+
+    document.getElementById("works-header").innerHTML = `${MEDIUM_KEYS[qs.category]} Works`;
+
+    // loop through the database and filter only works of that kind
+    const FILTERED_DATA = {}
+
+    for (const work in MASTER_DB.works) {
+        if (MASTER_DB.works[work].category == qs.category) {
+            FILTERED_DATA[work] = MASTER_DB.works[work]
+        }
+    }
+
+    const WORKS_TABLE = document.getElementById("works-table");
+
+    for (const workId in FILTERED_DATA) {
+        let row = WORKS_TABLE.insertRow();
+        let work = FILTERED_DATA[workId];
+
+        // display name
+        $(row).append(`<td><a href="/item?id=${workId}">${work.displayName}</a></th>`);
+
+        // archive date
+        let arcdate = new Date(work.archiveDate);
+        $(row).append(`<td><span title="${arcdate.toDateString()}" class="hastooltip">${work.archiveDate}</span></th>`);
+
+        // og date
+        let origdate = new Date(work.originalDate);
+        $(row).append(`<td><span title="${origdate.toDateString()}" class="hastooltip">${work.originalDate}</span></th>`);
+    }
+});
